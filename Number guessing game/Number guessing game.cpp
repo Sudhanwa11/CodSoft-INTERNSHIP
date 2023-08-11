29579fefcfb85a6f94ce66c62580f8f46dc1b496
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-08-11 19:28:59 +0530

    Create Number guessing game.cpp

---
+++ Number guessing game/Number guessing game.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand ((unsigned int) time(NULL));
+    int number = (rand() % 100) + 1; // 0-99 + 1 -> 1-100
+    int guess = 0;
+    
+    do {
+        cout << "Enter your guess (1-100): ";
+        cin >> guess;
+        if (guess > number) {
+            cout << "Guess Lower!" <<endl;
+        }
+        else if (guess < number) {
+            cout << "Guess Higher!" <<endl;
+        }  
+        else {
+            cout << "Victory!!! YOU WON" <<endl;
+        }
+    } while (guess != number);
+    
+    return 0;
+}
